2b950733185c31c833cd57a51f9d82cc3bc617f9
GhulamMustafa <mg.gamer149@gmail.com>
2024-10-24 07:17:14 -1200

    done

---
+++ react/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {link} from "react-router-dom"
+
+export const Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  );
+};
+
